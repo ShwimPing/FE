ab0b954c7920043202c68458c8c0184255dab3bb
jisooooooooooo <dlawltn0708@naver.com>
2024-09-02 21:02:17 +0900

    Feat: 닉네임 설정 화면 구현

--- src/screens/Signup/ProfileScreen.tsx
+++ src/screens/Signup/ProfileScreen.tsx
@@ -1,20 +1,131 @@
-// ProfileScreen.tsx
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../App';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import Svg, {Path, G, ClipPath, Rect, Defs} from 'react-native-svg';
+import {RootStackParamList} from '../../App';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>;
 
-const ProfileScreen: React.FC<Props> = ({ navigation }) => {
+const ProfileScreen: React.FC<Props> = ({navigation}) => {
+  const [nickname, setNickname] = useState('');
+  const [isNicknameValid, setIsNicknameValid] = useState(true);
+  const [nicknameMessage, setNicknameMessage] = useState('');
+
+  const handleCheckNickname = () => {
+    if (nickname === '헴깅이22') {
+      setIsNicknameValid(true);
+      setNicknameMessage('사용 가능한 닉네임입니다!');
+    } else {
+      setIsNicknameValid(false);
+      setNicknameMessage('이미 사용 중인 닉네임입니다.');
+    }
+  };
+
+  const handleNicknameChange = (text: string) => {
+    setNickname(text);
+    if (text === '') {
+      setNicknameMessage('');
+      setIsNicknameValid(true);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>프로필을 등록해주세요</Text>
-      {/* 프로필 등록 UI 구현 */}
-      <TouchableOpacity 
-        style={styles.nextButton} 
-        onPress={() => navigation.navigate('SignUpComplete')}
-      >
+      <View style={styles.headerContainer}>
+        <Text style={styles.greeting}>반가워요!</Text>
+        <Text style={styles.subtitle}>
+          쉼핑에서 사용할 프로필을 등록해주세요
+        </Text>
+      </View>
+
+      <View style={styles.profileImageContainer}>
+        <Image
+          source={require('../../../assets/images/profile.png')}
+          style={styles.profileImage}
+        />
+        <View style={styles.addIconContainer}>
+          <Svg width="25" height="24" viewBox="0 0 25 24" fill="none">
+            <G clipPath="url(#clip0_263_4834)">
+              <Path
+                d="M24.5 12C24.5 5.37258 19.1274 0 12.5 0C5.87258 0 0.5 5.37258 0.5 12C0.5 18.6274 5.87258 24 12.5 24C19.1274 24 24.5 18.6274 24.5 12Z"
+                fill="#222222"
+              />
+              <Path
+                d="M16.5 10.6667H13.8334V8.00002C13.8334 7.64642 13.6929 7.30727 13.4428 7.05719C13.1928 6.80717 12.8536 6.66669 12.5 6.66669C12.1464 6.66669 11.8073 6.80717 11.5572 7.05719C11.3072 7.30727 11.1667 7.64642 11.1667 8.00002L11.214 10.6667H8.50002C8.14642 10.6667 7.80727 10.8072 7.55719 11.0572C7.30717 11.3073 7.16669 11.6464 7.16669 12C7.16669 12.3536 7.30717 12.6928 7.55719 12.9428C7.80727 13.1929 8.14642 13.3334 8.50002 13.3334L11.214 13.286L11.1667 16C11.1667 16.3536 11.3072 16.6928 11.5572 16.9428C11.8073 17.1929 12.1464 17.3334 12.5 17.3334C12.8536 17.3334 13.1928 17.1929 13.4428 16.9428C13.6929 16.6928 13.8334 16.3536 13.8334 16V13.286L16.5 13.3334C16.8536 13.3334 17.1928 13.1929 17.4428 12.9428C17.6929 12.6928 17.8334 12.3536 17.8334 12C17.8334 11.6464 17.6929 11.3073 17.4428 11.0572C17.1928 10.8072 16.8536 10.6667 16.5 10.6667Z"
+                fill="white"
+              />
+            </G>
+            <Defs>
+              <ClipPath id="clip0_263_4834">
+                <Rect
+                  width="24"
+                  height="24"
+                  fill="white"
+                  transform="translate(0.5)"
+                />
+              </ClipPath>
+            </Defs>
+          </Svg>
+        </View>
+      </View>
+
+      <View style={styles.nicknameContainer}>
+        <Text style={styles.label}>닉네임</Text>
+        <View style={styles.inputRow}>
+          <TextInput
+            style={[
+              styles.nicknameInput,
+              {
+                borderColor: isNicknameValid ? '#F8F9FA' : '#FF5252',
+                borderWidth: isNicknameValid ? 0 : 1,
+              },
+            ]}
+            value={nickname}
+            onChangeText={handleNicknameChange}
+            placeholder="닉네임을 입력하세요"
+          />
+          <TouchableOpacity
+            style={[
+              styles.checkButton,
+              {
+                backgroundColor: nickname ? '#222' : '#F8F9FA',
+              },
+            ]}
+            onPress={handleCheckNickname}
+            disabled={!nickname}>
+            <Text
+              style={[
+                styles.checkButtonText,
+                {
+                  color: nickname ? '#FFF' : '#D2D3D3',
+                },
+              ]}>
+              중복 확인
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {nicknameMessage ? (
+          <Text
+            style={[
+              styles.nicknameMessage,
+              {color: isNicknameValid ? '#8E9398' : '#FF5252'},
+            ]}>
+            {nicknameMessage}
+          </Text>
+        ) : null}
+      </View>
+
+      <TouchableOpacity
+        style={styles.nextButton}
+        onPress={() => navigation.navigate('SignUpComplete')}>
         <Text style={styles.nextButtonText}>다음</Text>
       </TouchableOpacity>
     </View>
@@ -22,10 +133,111 @@ const ProfileScreen: React.FC<Props> = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  title: { fontSize: 18, fontFamily: 'Pretendard-Bold', marginBottom: 20 },
-  nextButton: { padding: 15, backgroundColor: '#ddd', borderRadius: 8 },
-  nextButtonText: { color: '#000' },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    paddingHorizontal: 16,
+    paddingTop: 62,
+  },
+  headerContainer: {
+    width: '100%',
+    alignItems: 'flex-start',
+    paddingHorizontal: 16,
+    marginBottom: 32,
+  },
+  greeting: {
+    fontSize: 18,
+    fontFamily: 'Pretendard-Bold',
+    color: '#1A1A1B',
+    marginBottom: 4,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontFamily: 'Pretendard-Bold',
+    color: '#1A1A1B',
+    marginBottom: 16,
+  },
+  profileImageContainer: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 36,
+  },
+  profileImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 50,
+  },
+  addIconContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  nicknameContainer: {
+    width: '100%',
+    paddingHorizontal: 16,
+    marginBottom: 16,
+  },
+  label: {
+    color: '#1A1A1B',
+    fontFamily: 'Pretendard',
+    fontSize: 14,
+    fontWeight: '600',
+    lineHeight: 21,
+    marginBottom: 8,
+  },
+  inputRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  nicknameInput: {
+    height: 52,
+    flex: 1,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    backgroundColor: '#F8F9FA',
+    fontFamily: 'Pretendard',
+    fontSize: 14,
+    fontWeight: '600',
+    lineHeight: 21,
+  },
+  checkButton: {
+    width: 68,
+    height: 52,
+    marginLeft: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  checkButtonText: {
+    fontSize: 14,
+    fontFamily: 'Pretendard-Bold',
+  },
+  nicknameMessage: {
+    marginTop: 8,
+    fontSize: 12,
+    fontFamily: 'Pretendard',
+    lineHeight: 18,
+  },
+  nextButton: {
+    width: '100%',
+    maxWidth: 343,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: '#F8F9FA',
+    position: 'absolute',
+    bottom: 57,
+  },
+  nextButtonText: {
+    fontSize: 14,
+    fontFamily: 'Pretendard-Bold',
+    color: '#D2D3D3',
+  },
 });
 
 export default ProfileScreen;
--- src/screens/Signup/SignupCompleteScreen.tsx
+++ src/screens/Signup/SignupCompleteScreen.tsx
@@ -1,7 +1,7 @@
-// SignUpCompleteScreen.tsx
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import Svg, { Path, Defs, LinearGradient, Stop } from 'react-native-svg';
 import { RootStackParamList } from '../../App';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'SignUpComplete'>;
@@ -9,22 +9,76 @@ type Props = NativeStackScreenProps<RootStackParamList, 'SignUpComplete'>;
 const SignUpCompleteScreen: React.FC<Props> = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>회원가입이 완료되었습니다!</Text>
-      <TouchableOpacity 
-        style={styles.nextButton} 
-        onPress={() => navigation.navigate('Login')}
-      >
-        <Text style={styles.nextButtonText}>시작하기</Text>
+      <Svg width="101" height="100" viewBox="0 0 101 100" fill="none">
+        <Path
+          d="M50.5 91.6666C73.5119 91.6666 92.1667 73.0118 92.1667 50C92.1667 26.9881 73.5119 8.33331 50.5 8.33331C27.4882 8.33331 8.83337 26.9881 8.83337 50C8.83337 73.0118 27.4882 91.6666 50.5 91.6666Z"
+          fill="url(#paint0_linear_263_4862)"
+        />
+        <Path
+          d="M31.75 50L44.25 62.5L69.25 37.5M92.1667 50C92.1667 73.0118 73.5119 91.6666 50.5 91.6666C27.4882 91.6666 8.83337 73.0118 8.83337 50C8.83337 26.9881 27.4882 8.33331 50.5 8.33331C73.5119 8.33331 92.1667 26.9881 92.1667 50Z"
+          stroke="white"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <Defs>
+          <LinearGradient
+            id="paint0_linear_263_4862"
+            x1="8.83337"
+            y1="50"
+            x2="92.1667"
+            y2="50"
+            gradientUnits="userSpaceOnUse">
+            <Stop stopColor="#222" />
+            <Stop offset="1" stopColor="#222" />
+          </LinearGradient>
+        </Defs>
+      </Svg>
+
+      <Text style={styles.message}>
+        헴깅이22 님,{'\n'}회원가입이 완료되었습니다!
+      </Text>
+
+      <TouchableOpacity
+        style={styles.startButton}
+        onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.startButtonText}>시작하기</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  title: { fontSize: 18, fontFamily: 'Pretendard-Bold', marginBottom: 20 },
-  nextButton: { padding: 15, backgroundColor: '#000', borderRadius: 8 },
-  nextButtonText: { color: '#fff' },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop:167,
+    backgroundColor: '#FFF',
+  },
+  message: {
+    fontSize: 18,
+    fontFamily: 'Pretendard-Bold',
+    color: '#1A1A1B',
+    textAlign: 'center',
+    marginVertical: 12,
+  },
+  startButton: {
+    width: '100%',
+    maxWidth: 343,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: '#222',
+    position: 'absolute',
+    bottom: 57,
+  },
+  startButtonText: {
+    fontSize: 14,
+    lineHeight:21,
+    fontFamily: 'Pretendard-Bold',
+    color: '#FFF',
+  },
 });
 
 export default SignUpCompleteScreen;
