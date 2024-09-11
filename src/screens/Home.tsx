03b2f26d8b206e924b6397347c5933daf4c154ba
jisooooooooooo <dlawltn0708@naver.com>
2024-09-11 23:00:23 +0900

    Feat: 현위치 이동 구현

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   View,
   TextInput,
@@ -8,12 +8,13 @@ import {
   Alert,
   TouchableOpacity,
 } from 'react-native';
-import Svg, {Path, G, ClipPath, Defs, Rect} from 'react-native-svg';
+import Svg, {Path, G, Defs, ClipPath, Rect} from 'react-native-svg';
 import Footer from '../components/Footer';
 import NaverMapView, {Marker} from 'react-native-nmap';
 import Geolocation from 'react-native-geolocation-service';
 
 const Home = () => {
+  const mapViewRef = useRef<NaverMapView>(null);
   const [location, setLocation] = useState({
     latitude: 37.5665,
     longitude: 126.978,
@@ -39,11 +40,14 @@ const Home = () => {
     const getCurrentLocation = () => {
       Geolocation.getCurrentPosition(
         position => {
-          console.log('Current Position:', position);
           setLocation({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
             zoom: 16,
+          });
+          mapViewRef.current?.animateToCoordinate({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
           });
         },
         error => {
@@ -67,6 +71,11 @@ const Home = () => {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
           zoom: 16,
+        });
+
+        mapViewRef.current?.animateToCoordinate({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
         });
       },
       error => {
@@ -115,20 +124,12 @@ const Home = () => {
             placeholder="검색어를 입력해주세요."
             placeholderTextColor="#8E9398"
           />
-          <Svg width="20" height="21" viewBox="0 0 20 21" fill="none">
-            <Path
-              d="M15.8333 8.83342V10.5001C15.8333 13.7217 13.2216 16.3334 9.99996 16.3334M4.16663 8.83342V10.5001C4.16663 13.7217 6.7783 16.3334 9.99996 16.3334M9.99996 16.3334V18.8334M6.66663 18.8334H13.3333M9.99996 13.0001C8.61925 13.0001 7.49996 11.8808 7.49996 10.5001V4.66675C7.49996 3.28604 8.61925 2.16675 9.99996 2.16675C11.3807 2.16675 12.5 3.28604 12.5 4.66675V10.5001C12.5 11.8808 11.3807 13.0001 9.99996 13.0001Z"
-              stroke="#1A1A1B"
-              strokeWidth="1.1"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </Svg>
         </View>
       </View>
 
       <View style={styles.mapContainer}>
         <NaverMapView
+          ref={mapViewRef} // ref를 NaverMapView에 연결
           style={styles.map}
           center={{
             latitude: location.latitude,
@@ -143,24 +144,25 @@ const Home = () => {
           }}
           pinColor="blue"
         />
-        <TouchableOpacity style={styles.currentLocationButton} onPress={moveToCurrentLocation}>
-          <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <Path
-              d="M12 2V4M12 20V22M4.93 4.93L6.34 6.34M17.66 17.66L19.07 19.07M2 12H4M20 12H22M4.93 19.07L6.34 17.66M17.66 6.34L19.07 4.93"
-              stroke="#000"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <Path
-              d="M12 8C14.2091 8 16 9.79086 16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8Z"
-              stroke="#000"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </Svg>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <Path d="M4.16663 6.5C4.16663 5.09987 4.16663 4.3998 4.43911 3.86502C4.67879 3.39462 5.06124 3.01217 5.53165 2.77248C6.06643 2.5 6.76649 2.5 8.16663 2.5H11.8333C13.2334 2.5 13.9335 2.5 14.4683 2.77248C14.9387 3.01217 15.3211 3.39462 15.5608 3.86502C15.8333 4.3998 15.8333 5.09987 15.8333 6.5V17.5L9.99996 14.1667L4.16663 17.5V6.5Z" stroke="#222222" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round"/>
+            </Svg>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={moveToCurrentLocation}>
+            <Svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <G clipPath="url(#clip0_297_770)">
+                <Path d="M10 0C8.02219 0 6.08879 0.58649 4.4443 1.6853C2.79981 2.78412 1.51809 4.3459 0.761209 6.17317C0.00433286 8.00043 -0.193701 10.0111 0.192152 11.9509C0.578004 13.8907 1.53041 15.6725 2.92894 17.0711C4.32746 18.4696 6.10929 19.422 8.0491 19.8079C9.98891 20.1937 11.9996 19.9957 13.8268 19.2388C15.6541 18.4819 17.2159 17.2002 18.3147 15.5557C19.4135 13.9112 20 11.9778 20 10C19.9971 7.34872 18.9426 4.80684 17.0679 2.9321C15.1932 1.05736 12.6513 0.00286757 10 0V0ZM10.8333 18.2908V16.6667C10.8333 16.4457 10.7455 16.2337 10.5893 16.0774C10.433 15.9211 10.221 15.8333 10 15.8333C9.77899 15.8333 9.56703 15.9211 9.41075 16.0774C9.25447 16.2337 9.16667 16.4457 9.16667 16.6667V18.2908C7.25515 18.0962 5.46932 17.248 4.11068 15.8893C2.75204 14.5307 1.9038 12.7449 1.70917 10.8333H3.33334C3.55435 10.8333 3.76631 10.7455 3.92259 10.5893C4.07887 10.433 4.16667 10.221 4.16667 10C4.16667 9.77899 4.07887 9.56703 3.92259 9.41074C3.76631 9.25447 3.55435 9.16667 3.33334 9.16667H1.70917C1.9038 7.25514 2.75204 5.46932 4.11068 4.11068C5.46932 2.75203 7.25515 1.9038 9.16667 1.70917V3.33333C9.16667 3.55435 9.25447 3.76631 9.41075 3.92259C9.56703 4.07887 9.77899 4.16667 10 4.16667C10.221 4.16667 10.433 4.07887 10.5893 3.92259C10.7455 3.76631 10.8333 3.55435 10.8333 3.33333V1.70917C12.7449 1.9038 14.5307 2.75203 15.8893 4.11068C17.248 5.46932 18.0962 7.25514 18.2908 9.16667H16.6667C16.4457 9.16667 16.2337 9.25447 16.0774 9.41074C15.9211 9.56703 15.8333 9.77899 15.8333 10C15.8333 10.221 15.9211 10.433 16.0774 10.5893C16.2337 10.7455 16.4457 10.8333 16.6667 10.8333H18.2908C18.0962 12.7449 17.248 14.5307 15.8893 15.8893C14.5307 17.248 12.7449 18.0962 10.8333 18.2908ZM13.3333 10C13.3333 10.221 13.2455 10.433 13.0893 10.5893C12.933 10.7455 12.721 10.8333 12.5 10.8333H10.8333V12.5C10.8333 12.721 10.7455 12.933 10.5893 13.0893C10.433 13.2455 10.221 13.3333 10 13.3333C9.77899 13.3333 9.56703 13.2455 9.41075 13.0893C9.25447 12.933 9.16667 12.721 9.16667 12.5V10.8333H7.5C7.27899 10.8333 7.06703 10.7455 6.91075 10.5893C6.75447 10.433 6.66667 10.221 6.66667 10C6.66667 9.77899 6.75447 9.56703 6.91075 9.41074C7.06703 9.25447 7.27899 9.16667 7.5 9.16667H9.16667V7.5C9.16667 7.27899 9.25447 7.06703 9.41075 6.91074C9.56703 6.75446 9.77899 6.66667 10 6.66667C10.221 6.66667 10.433 6.75446 10.5893 6.91074C10.7455 7.06703 10.8333 7.27899 10.8333 7.5V9.16667H12.5C12.721 9.16667 12.933 9.25447 13.0893 9.41074C13.2455 9.56703 13.3333 9.77899 13.3333 10Z" fill="#222222"/>
+              </G>
+              <Defs>
+                <ClipPath id="clip0_297_770">
+                  <Rect width="20" height="20" fill="white"/>
+                </ClipPath>
+              </Defs>
+            </Svg>
+          </TouchableOpacity>
+        </View>
       </View>
 
       <Footer />
@@ -207,21 +209,29 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-  currentLocationButton: {
+  buttonContainer: {
     position: 'absolute',
-    bottom: 20,
     right: 20,
-    width: 50,
-    height: 50,
-    backgroundColor: '#fff',
-    borderRadius: 25,
-    justifyContent: 'center',
+    bottom: 100,
+    flexDirection: 'column',
     alignItems: 'center',
-    elevation: 5,
+    gap: 10,
+  },
+  button: {
+    display: 'flex',
+    padding: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#D2D3D3',
+    backgroundColor: '#FFF',
     shadowColor: '#000',
     shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.3,
     shadowRadius: 3.84,
+    elevation: 5,
+    marginBottom: 10,
   },
 });
 
